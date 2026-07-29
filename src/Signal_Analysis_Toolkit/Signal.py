6d3e6e3980bb2757c10dd9e9e56cfa974bd57391
GiannosX <xristoulakis.i@gmail.com>
2026-07-29 11:03:39 +0300

    Square Signal fix

--- src/Signal_Analysis_Toolkit/Signal.py
+++ src/Signal_Analysis_Toolkit/Signal.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-class Signal: 
+class Signal:  # Parent class 
     def __init__(self, duration: float, sample_rate: int, amplitude: int = 1, frequency: int = 1, phase: int = 0):
         self.duration = duration 
         self.sample_rate = sample_rate
@@ -9,10 +9,10 @@ class Signal:
         self.frequency = frequency
         self.phase = phase
 
-    def generate(self): 
+    def generate(self): # returns the signal array 
         pass
 
-    @property 
+    @property # Property that gives the time array for plotting
     def samples(self): 
         return np.linspace(0,self.duration, self.sample_rate*self.duration)
 
@@ -31,10 +31,11 @@ class SquareSignal(Signal):
 
     def generate(self): 
         x = self.samples
-        square = [self.amplitude for _ in x if _<_%self.frequency else -self.amplitude]
+        square = [self.amplitude if i%self.frequency<self.frequency/2 else -self.amplitude for i in x]
+        return np.array(square)
 
 
-y = SquareSignal(5, 100, frequency=2)
+y = SquareSignal(10, 100, frequency=2)
 t = y.samples
 
 fig ,ax = plt.subplots(figsize = (16,9))
